07a8ca429840da822ec16c0c24ab286b2495ad14
Badhon-Roy <roybadhon286@gmail.com>
2025-03-24 23:54:46 +0600

    some change

--- src/components/modules/home/FeaturedProducts/index.tsx
+++ src/components/modules/home/FeaturedProducts/index.tsx
@@ -6,7 +6,7 @@ import { IListing } from "@/types";
 import Link from "next/link";
 
 const FeaturedProducts = async ({ listings }: { listings: IListing[] }) => {
- 
+
     return (
         <div>
             <div className="text-center">
@@ -20,13 +20,13 @@ const FeaturedProducts = async ({ listings }: { listings: IListing[] }) => {
             <div className="flex justify-between items-center">
                 <h2 className="md:text-2xl text-xl font-bold">Featured Products</h2>
                 <Link href={'/products'}>
-                <Button className="bg-gradient-to-r from-[#ffbe0c] to-[#ff8e00] md:px-8 md:py-6 px-2 md:text-[18px]  rounded-[4px] text-white font-semibold shadow-md transform transition-transform duration-300 hover:scale-105 hover:from-[#e9a912] hover:to-[#ff6f00] hover:shadow-lg active:scale-95 focus:outline-none cursor-pointer">
-                    View All
-                </Button></Link>
+                    <Button className="bg-gradient-to-r from-[#ffbe0c] to-[#ff8e00] md:px-8 md:py-6 px-2 md:text-[18px]  rounded-[4px] text-white font-semibold shadow-md transform transition-transform duration-300 hover:scale-105 hover:from-[#e9a912] hover:to-[#ff6f00] hover:shadow-lg active:scale-95 focus:outline-none cursor-pointer">
+                        View All
+                    </Button></Link>
             </div>
             <div className="grid xl:grid-cols-4 lg:grid-cols-3 md:grid-cols-2 grid-cols-2 md:gap-6 gap-3 my-8">
                 {
-                    listings?.slice(0,8)?.map((listing: IListing) => (
+                    listings?.slice(0, 8)?.map((listing: IListing) => (
                         <ProductCard key={listing?._id} product={listing} />
                     ))
                 }
--- src/components/modules/home/banner/index.tsx
+++ src/components/modules/home/banner/index.tsx
@@ -18,16 +18,16 @@ const Banner = () => {
                     <div className="flex-1 lg:mt-0 mt-4">
                         <h2 className="xl:text-[70px] md:text-[50px] text-[35px] font-bold lg:leading-16 md:leading-14 leading-8 md:text-start text-center"><span className="text-[#ff8e00]">Buy</span> & <span className="text-[#ff8e00]">Sell</span> <br /><span className="xl:text-[40px] md:text-[30px] text-[20px]"> Pre-loved Treasures</span></h2>
                         <p className="md:text-xl text-sm font-medium my-4">Discover a marketplace where you can buy and sell gently used items. Whether you&apos;re looking for a great deal or wanting to declutter, SecondHand makes it easy to give items a second life. Join today and find your next treasure!</p>
-                        <Link href={'/products'}> 
-                        <Button className="bg-gradient-to-r from-[#ffbe0c] to-[#ff8e00] px-8 py-6 rounded-[4px] text-white font-semibold text-[18px] shadow-md transform transition-transform duration-300 hover:scale-105 hover:from-[#e9a912] hover:to-[#ff6f00] hover:shadow-lg active:scale-95 focus:outline-none cursor-pointer">
-                            Shop Now
-                        </Button></Link>
+                        <Link href={'/products'}>
+                            <Button className="bg-gradient-to-r from-[#ffbe0c] to-[#ff8e00] px-8 py-6 rounded-[4px] text-white font-semibold text-[18px] shadow-md transform transition-transform duration-300 hover:scale-105 hover:from-[#e9a912] hover:to-[#ff6f00] hover:shadow-lg active:scale-95 focus:outline-none cursor-pointer">
+                                Shop Now
+                            </Button></Link>
                     </div>
                 </div>
             </div>
             <div className="lg:w-[20%] flex lg:flex-col xl:justify-between md:gap-8 gap-4 lg:mt-0 mt-4">
                 <div className="border-2 border-[#ff8e00] p-2 rounded-lg shadow-lg">
-                    <Image  className="rounded-lg object-cover w-full 2xl:h-full xl:h-[200px] h-full" src="https://www.itaf.eu/wp-content/uploads/2021/01/xBest-laptops-in-2021-7-things-to-consider-when-buying-a-laptop.jpg.pagespeed.ic.9ZN_pp1y3L.jpg" alt="offer_image" width={500} height={400}></Image>
+                    <Image className="rounded-lg object-cover w-full 2xl:h-full xl:h-[200px] h-full" src="https://www.itaf.eu/wp-content/uploads/2021/01/xBest-laptops-in-2021-7-things-to-consider-when-buying-a-laptop.jpg.pagespeed.ic.9ZN_pp1y3L.jpg" alt="offer_image" width={500} height={400}></Image>
                 </div>
                 <div className="border-2 border-[#ff8e00] p-2 rounded-lg shadow-lg">
                     <Image className="rounded-lg object-cover w-full" src={OfferImage} alt="offer_image" width={500} height={400}></Image>
